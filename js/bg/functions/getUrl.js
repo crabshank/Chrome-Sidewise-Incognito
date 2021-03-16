de8667737f4f587bf21a87763862c4a43f76099b
crabshank <44329154+crabshank@users.noreply.github.com>
2021-03-16 02:42:53 +0000

    Add files via upload

--- js/bg/functions/getUrl.js
+++ js/bg/functions/getUrl.js
@@ -1,13 +1,3 @@
 function getUrl(tab){
-	if(tab.pendingUrl!=null){
-	return (tab.url=="")?tab.pendingUrl:tab.url;
-	}else{
-		
-		if(tab.url!=null){
-			return tab.url;
-		}else{
-			return tab; //if tab.url not there
-		}
-	
-	}
+	return (tab.url=="" && typeof tab.pendingUrl!=='undefined' && tab.pendingUrl!='')?tab.pendingUrl:tab.url;
 }
