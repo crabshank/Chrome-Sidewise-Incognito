e188c94387f4c2a6f4e074687420823e95c62f7a
crabshank <44329154+crabshank@users.noreply.github.com>
2021-03-17 16:25:38 +0000

    Update getUrl.js

--- js/bg/functions/getUrl.js
+++ js/bg/functions/getUrl.js
@@ -1,3 +1,3 @@
 function getUrl(tab){
-	return (tab.url=="" && typeof tab.pendingUrl!=='undefined' && tab.pendingUrl!='')?tab.pendingUrl:tab.url;
+	return (tab.url=="" && !!tab.pendingUrl && typeof tab.pendingUrl!=='undefined' && tab.pendingUrl!='')?tab.pendingUrl:tab.url;
 }
