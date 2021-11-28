f484cf21874585f0c289ced535fe912ab149684a
crabshank <44329154+crabshank@users.noreply.github.com>
2021-11-28 17:59:26 +0000

    Delete js directory

--- js/bg/events/snapin-events.js
+++
@@ -1,9 +0,0 @@
-function registerSnapInEvents() {
-    chrome.extension.onRequestExternal.addListener(onRequestExternal);
-}
-
-function onRequestExternal(request, sender, sendResponse) {
-    sidebarHandler.sidebarPanes['sidebarHost'].manager.addSidebarPane(
-        sender.id, request.label, request.icon, request.url);
-}
-
